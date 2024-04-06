09070cf26918c76752e643c450482b09d6baa79c
M0xX25 <andreseduardo.012@gmail.com>
2024-04-06 00:24:40 -0500

    Trabajos Universidad

--- ActividadesUni/BuynSell/Main/app/views/custom/footer.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="<?php echo URL_CSS?>/footer.css">
-<footer class="footer">
-		<p>Derechos reservados © 2023. BuynSell.</p>
-		<a href="#">Términos y condiciones</a>
-		<a href="#">Política de privacidad</a>
-		<a href="<?php echo URL_VIEWS?>/directoriotel.php">Contacto</a>
-	</footer>
--- Proyectos/DesarrolloRest/PythonCrud/autos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE vehiculo (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    modelo VARCHAR(255),
-    fabricante VARCHAR(255) NOT NULL,
-    país VARCHAR(255) NOT NULL
-);
